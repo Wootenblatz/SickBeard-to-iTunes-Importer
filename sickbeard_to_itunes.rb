4302aae1765a7c4c8806a24c458d399f4f0852ee
Zach Karpinski <zkarpinski@gmail.com>
2012-05-08 20:34:19 -0500

    Updated readme; Cleaned up and commented code and the name parsing system; Added yaml config file

--- sickbeard_to_itunes.rb
+++ sickbeard_to_itunes.rb
@@ -1,26 +1,15 @@
-# Path to your SickBeard library
-sb_lib = "/Volumes/500GB/Sick\\ Beard"
+require 'yaml'
 
-# Path to your iTunes library
-it_lib = "/Volumes/2TB/iTunes"
+config = YAML.load_file("config.yaml")
+sickbeard_download_location = config["sickbeard_download_location"]
+itunes_path = config["itunes_path"]
+organized_itunes = config["organized_itunes"]
+path_to_handbrake = config["path_to_handbrake"]
+temporary_output_path = config["temporary_output_path"]
+video_quality_video_quality_preset = config["video_quality_video_quality_preset"]
+handbrake_options = config["handbrake_options"]
+nice_command = config["nice_command"]
 
-# iTunes copies imported files
-organized_itunes = true
-
-# Where you have HandBrakeCLI installed
-pathToHB = "/Applications/"
-
-# Temporary path your m4v files should end up before being imported to iTunes
-outPath = "/Users/zach/Movies/"
-
-# Target quality for HandBrake
-preset = "AppleTV 2"
-
-# Other options for HandBrake
-options = ""
-
-# Unix priority 19 will give up priority so your computer isn't _as_ slow
-nice = "/usr/bin/nice -n 19"
 
 # The code, no need to edit any of this unless you know what you're doing!
 def get_filename(path)
@@ -51,11 +40,11 @@ while 1 == 1
 
   file_list = Array.new
 
-  `find #{sb_lib} -iname *\.mkv -o -iname *\.avi -o -iname *\.wmv`.each do |f|
+  `find #{sickbeard_download_location} -iname *\.mkv -o -iname *\.avi -o -iname *\.wmv`.each do |f|
     filename = get_filename(f)
     info = create_show_details(filename)
     #  This checks for new style names and old style names
-    i = `find #{it_lib} -iname "*#{filename.gsub(/\W/,"*")}*"`.chomp.size + `find #{it_lib} -iname "*#{get_title(info)}*"`.chomp.size
+    i = `find #{itunes_path} -iname "*#{filename.gsub(/\W/,"*")}*"`.chomp.size + `find #{itunes_path} -iname "*#{get_title(info)}*"`.chomp.size
     if i == 0
        file_list.push(f.chomp)
     end
@@ -63,34 +52,42 @@ while 1 == 1
 
   file_list.each do |inPath|
       filename = get_filename(inPath)
-      outPathWName = outPath + filename.gsub(/\W/,"_") + ".m4v"
+      temporary_output_path_with_name = temporary_output_path + filename.gsub(/\W/,"_") + ".m4v"
       inPath.chomp!
-      system "#{nice} #{pathToHB}HandBrakeCLI -i \"#{inPath}\" -o \"#{outPathWName}\" --preset=\"#{preset}\" #{options}"
-      outPathWName.gsub!(/\'/,"\\'")
-      track_id = `/usr/bin/osascript -e 'tell application \"iTunes\" to add POSIX file \"#{outPathWName}\"'`.chomp.split(" ")[3]
-      info = create_show_details(filename)
-      
-      # Set the episode's title
-      system "/usr/bin/osascript -e 'tell application \"iTunes\" to set name of track id #{track_id} to \"#{get_title(info)}\"'"
-      
-      # Change the Media Kind to TV Show
-      system "/usr/bin/osascript -e 'tell application \"iTunes\" to set video kind of track id #{track_id} to tv show'"
-      
-      # Set the season number
-      system "/usr/bin/osascript -e 'tell application \"iTunes\" to set season number of track id #{track_id} to #{get_season_number(info)}'"
-      
-      # Set the episode number
-      system "/usr/bin/osascript -e 'tell application \"iTunes\" to set episode number of track id #{track_id} to #{get_episode_number(info)}'"
-      
-      # Set the name of the show
-      system "/usr/bin/osascript -e 'tell application \"iTunes\" to set show of track id #{track_id} to \"#{get_show_name(info)}\"'"
-      
-      sleep 10
-      
-      # If the initial file addition was successful, itunes copied everything and we can delete our temporary file
-      if organized_itunes and track_id.to_i > 0
-        File.delete(outPathWName)
+      system "#{nice_command} #{path_to_handbrake}HandBrakeCLI -i \"#{inPath}\" -o \"#{temporary_output_path_with_name}\" --preset=\"#{video_quality_preset}\" #{handbrake_options}"
+      temporary_output_path_with_name.gsub!(/\'/,"\\'")
+      track_id = `/usr/bin/osascript -e 'tell application \"iTunes\" to add POSIX file \"#{temporary_output_path_with_name}\"'`.chomp.split(" ")[3]
+
+      begin
+	info = create_show_details(filename)
+      rescue Exception => e
+	puts "\n\n\n!!! Was not able to process #{filename} !!!\n\n"
+	puts e.message
       end
+
+      if info
+      	# Set the episode's title
+      	system "/usr/bin/osascript -e 'tell application \"iTunes\" to set name of track id #{track_id} to \"#{get_title(info)}\"'"
+
+      	# Change the Media Kind to TV Show
+      	system "/usr/bin/osascript -e 'tell application \"iTunes\" to set video kind of track id #{track_id} to tv show'"
+
+      	# Set the season number
+      	system "/usr/bin/osascript -e 'tell application \"iTunes\" to set season number of track id #{track_id} to #{get_season_number(info)}'"
+
+      	# Set the episode number
+      	system "/usr/bin/osascript -e 'tell application \"iTunes\" to set episode number of track id #{track_id} to #{get_episode_number(info)}'"
+
+      	# Set the name of the show
+      	system "/usr/bin/osascript -e 'tell application \"iTunes\" to set show of track id #{track_id} to \"#{get_show_name(info)}\"'"
+
+      	sleep 10
+
+      	# If the initial file addition was successful, itunes copied everything and we can delete our temporary file
+      	if organized_itunes and track_id.to_i > 0
+	File.delete(temporary_output_path_with_name)
+      end
+    end
   end
 
   if file_list.size == 0
